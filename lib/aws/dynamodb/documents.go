9cd8ae8488d8f45bc61f90dfd49289fe36000c37
NJRodriguez <nicolasjrodriguez1@gmail.com>
2020-09-27 19:36:34 -0300

    Add client for interacting with dynamodb

---
+++ lib/aws/dynamodb/documents.go
@@ -0,0 +1,48 @@
+package dynamodb
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+)
+
+type documents struct {
+	awsDynamodbClient dynamodbiface.DynamoDBAPI
+	table             string
+}
+
+var newAwsSession = session.NewSession
+
+func New(table string, awsRegion string) (*documents, error) {
+	session, err := newAwsSession(&aws.Config{Region: aws.String(awsRegion)})
+	if err != nil {
+		return nil, err
+	}
+	service := dynamodb.New(session)
+	return &documents{service, table}, nil
+}
+
+func (instance *documents) Get(key map[string]*dynamodb.AttributeValue) (*dynamodb.GetItemOutput, error) {
+	args := &dynamodb.GetItemInput{
+		TableName: aws.String(instance.table),
+		Key:       key,
+	}
+	item, err := instance.awsDynamodbClient.GetItem(args)
+	if err != nil {
+		return nil, err
+	}
+	return item, nil
+}
+
+func (instance *documents) Create(item map[string]*dynamodb.AttributeValue) (*dynamodb.PutItemOutput, error) {
+	args := &dynamodb.PutItemInput{
+		TableName: aws.String(instance.table),
+		Item:      item,
+	}
+	result, err := instance.awsDynamodbClient.PutItem(args)
+	if err != nil {
+		return nil, err
+	}
+	return result, err
+}
